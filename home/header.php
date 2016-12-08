72299fd37a9fa0ed360c509f680b4da339d82b4e
David West <davidwest@davids-mbp.intra.ecu.edu>
2016-12-07 20:32:21 -0500

    forked the header and footer out into separate files to include in the multiple pages

---
+++ home/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Grey Side | Home</title>
+    <script type="text/javaScript" src="../jquery-3.1.1.min.js"></script>
+    <script type="text/javaScript" src="../greyscript.js"></script>
+    <script type="text/javascript" src="bootstrap.min.js"></script>
+    <script type="text/javaScript"src="script.js"></script>
+    <link rel="stylesheet" type="text/css" href="Global.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="../Grey Side.png" type="image/png">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
--- home/index.php
+++ home/index.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Grey Side | Home</title>
-  <script type="text/javaScript" src="../jquery-3.1.1.min.js"></script>
-  <script type="text/javaScript" src="../greyscript.js"></script>
-    <script type="text/javascript" src="bootstrap.min.js"></script>
-  <script type="text/javaScript"src="script.js"></script>
-  <link rel="stylesheet" type="text/css" href="Global.css">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <link rel="shortcut icon" href="../Grey Side.png" type="image/png">
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+<?php include('header.php'); ?>
+
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
       <div class="navbar-header">
@@ -91,5 +79,4 @@
     </div>
 </div>
 
-</body>
-</html>
+<?php include('header.php'); ?>
